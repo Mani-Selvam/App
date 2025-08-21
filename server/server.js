3c9b01875a5ab8660d38cc32f5eba75badb23db0
Mani Selvam <144051889+Mani-Selvam@users.noreply.github.com>
2025-08-21 19:39:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -67,6 +67,10 @@ app.delete("/api/todos/:id", async (req, res) => {
         res.status(500).json({ error: "Server error" });
     }
 });
+app.use(express.static(path.join(__dirname, "../client/build")));
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
